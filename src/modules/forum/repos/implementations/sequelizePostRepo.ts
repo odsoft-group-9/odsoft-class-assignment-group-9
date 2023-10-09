aa474c38cabc90e0bd807f2ce6fa11b480ae1b9d
Simao Santos <1230212@isep.ipp.pt>
2023-10-09 11:52:27 +0100

    feature: Update new posts listing to count only first level comments

--- src/modules/forum/repos/implementations/sequelizePostRepo.ts
+++ src/modules/forum/repos/implementations/sequelizePostRepo.ts
@@ -1,173 +1,195 @@
-
-import { IPostRepo } from "../postRepo";
-import { PostId } from "../../domain/postId";
-import { Post } from "../../domain/post";
-import { PostMap } from "../../mappers/postMap";
-import { PostDetails } from "../../domain/postDetails";
-import { PostDetailsMap } from "../../mappers/postDetailsMap";
-import { ICommentRepo } from "../commentRepo";
-import { IPostVotesRepo } from "../postVotesRepo";
-import { PostVotes } from "../../domain/postVotes";
-import { MemberId } from "../../domain/memberId";
-import { Comments } from "../../domain/comments";
+import {IPostRepo} from "../postRepo";
+import {PostId} from "../../domain/postId";
+import {Post} from "../../domain/post";
+import {PostMap} from "../../mappers/postMap";
+import {PostDetails} from "../../domain/postDetails";
+import {PostDetailsMap} from "../../mappers/postDetailsMap";
+import {ICommentRepo} from "../commentRepo";
+import {IPostVotesRepo} from "../postVotesRepo";
+import {PostVotes} from "../../domain/postVotes";
+import {MemberId} from "../../domain/memberId";
+import {Comments} from "../../domain/comments";
 
 export class PostRepo implements IPostRepo {
 
-  private models: any;
-  private commentRepo: ICommentRepo;
-  private postVotesRepo: IPostVotesRepo;
+    private models: any;
+    private commentRepo: ICommentRepo;
+    private postVotesRepo: IPostVotesRepo;
 
-  constructor (models: any, commentRepo: ICommentRepo, postVotesRepo: IPostVotesRepo) {
-    this.models = models;
-    this.commentRepo = commentRepo;
-    this.postVotesRepo = postVotesRepo;
-  }
-
-  private createBaseQuery (): any {
-    const models = this.models;
-    return {
-      where: {},
-      include: []
+    constructor(models: any, commentRepo: ICommentRepo, postVotesRepo: IPostVotesRepo) {
+        this.models = models;
+        this.commentRepo = commentRepo;
+        this.postVotesRepo = postVotesRepo;
     }
-  }
 
-  private createBaseDetailsQuery (): any {
-    const models = this.models;
-    return {
-      where: {},
-      include: [
-        { 
-          model: models.Member, 
-          as: 'Member', 
-          include: [
-            { model: models.BaseUser, as: 'BaseUser' }
-          ] 
+    private createBaseQuery(): any {
+        const models = this.models;
+        return {
+            where: {},
+            include: []
         }
-      ],
-      limit: 15,
-      offset: 0
     }
-  }
 
-  public async getPostByPostId (postId: PostId | string): Promise<Post> {
-    postId  = postId instanceof PostId 
-    ? (<PostId>postId).id.toString() 
-    : postId;
-    const PostModel = this.models.Post;
-    const detailsQuery = this.createBaseQuery();
-    detailsQuery.where['post_id'] = postId;
-    const post = await PostModel.findOne(detailsQuery);
-    const found = !!post === true;
-    if (!found) throw new Error("Post not found");
-    return PostMap.toDomain(post);
-  }
-
-  public async getNumberOfCommentsByPostId (postId: PostId | string): Promise<number> {
-    postId  = postId instanceof PostId 
-    ? (<PostId>postId).id.toString() 
-    : postId;
-
-    const result = await this.models.sequelize.query(
-      `SELECT COUNT(*) FROM comment WHERE post_id = "${postId}";`
-    );
-    const count = result[0][0]['COUNT(*)'];
-    return count;
-  }
-
-  public async getPostDetailsBySlug (slug: string, offset?: number): Promise<PostDetails> {
-    const PostModel = this.models.Post;
-    const detailsQuery = this.createBaseDetailsQuery();
-    detailsQuery.where['slug'] = slug;
-    const post = await PostModel.findOne(detailsQuery);
-    const found = !!post === true;
-    if (!found) throw new Error("Post not found");
-    return PostDetailsMap.toDomain(post)
-  }
-
-  public async getRecentPosts (offset?: number): Promise<PostDetails[]> {
-    const PostModel = this.models.Post;
-    const detailsQuery = this.createBaseDetailsQuery();
-    detailsQuery.offset = offset ? offset : detailsQuery.offset;
-    
-    const posts = await PostModel.findAll(detailsQuery);
-    return posts.map((p) => PostDetailsMap.toDomain(p))
-  }
-
-  public async getPopularPosts (offset?: number): Promise<PostDetails[]> {
-    const PostModel = this.models.Post;
-    const detailsQuery = this.createBaseDetailsQuery();
-    detailsQuery.offset = offset ? offset : detailsQuery.offset;
-    detailsQuery['order'] = [
-      ['points', 'DESC'],
-    ];
-
-    const posts = await PostModel.findAll(detailsQuery);
-    return posts.map((p) => PostDetailsMap.toDomain(p))
-  }
-
-  public async getPostBySlug (slug: string): Promise<Post> {
-    const PostModel = this.models.Post;
-    const detailsQuery = this.createBaseQuery();
-    detailsQuery.where['slug'] = slug;
-    const post = await PostModel.findOne(detailsQuery);
-    const found = !!post === true;
-    if (!found) throw new Error("Post not found");
-    return PostMap.toDomain(post);
-  }
-
-  public async exists (postId: PostId): Promise<boolean> {
-    const PostModel = this.models.Post;
-    const baseQuery = this.createBaseQuery();
-    baseQuery.where['post_id'] = postId.id.toString();
-    const post = await PostModel.findOne(baseQuery);
-    const found = !!post === true;
-    return found;
-  }
-
-  public delete (postId: PostId): Promise<void> {
-    const PostModel = this.models.Post;
-    return PostModel.destroy({ where: { post_id: postId.id.toString() }});
-  }
-
-  private saveComments (comments: Comments) {
-    return this.commentRepo.saveBulk(comments.getItems());
-  }
-
-  private savePostVotes (postVotes: PostVotes) {
-    return this.postVotesRepo.saveBulk(postVotes);
-  }
-
-  public async save (post: Post): Promise<void> {
-    const PostModel = this.models.Post;
-    const exists = await this.exists(post.postId);
-    const isNewPost = !exists;
-    const rawSequelizePost = await PostMap.toPersistence(post);
-    
-    if (isNewPost) {
-
-      try {
-        await PostModel.create(rawSequelizePost);
-        await this.saveComments(post.comments);
-        await this.savePostVotes(post.getVotes());
-        
-      } catch (err) {
-        await this.delete(post.postId);
-        throw new Error(err.toString())
-      }
-
-    } else {
-      // Save non-aggregate tables before saving the aggregate
-      // so that any domain events on the aggregate get dispatched
-      await this.saveComments(post.comments);
-      await this.savePostVotes(post.getVotes());
-      
-      await PostModel.update(rawSequelizePost, { 
-        // To make sure your hooks always run, make sure to include this in
-        // the query
-        individualHooks: true,  
-        hooks: true,
-        where: { post_id: post.postId.id.toString() }
-      });
+    private createBaseDetailsQuery(): any {
+        const models = this.models;
+        return {
+            where: {},
+            include: [
+                {
+                    model: models.Member,
+                    as: 'Member',
+                    include: [
+                        {model: models.BaseUser, as: 'BaseUser'}
+                    ]
+                }
+            ],
+            limit: 15,
+            offset: 0
+        }
     }
-  }
+
+    public async getPostByPostId(postId: PostId | string): Promise<Post> {
+        postId = postId instanceof PostId
+            ? (<PostId>postId).id.toString()
+            : postId;
+        const PostModel = this.models.Post;
+        const detailsQuery = this.createBaseQuery();
+        detailsQuery.where['post_id'] = postId;
+        const post = await PostModel.findOne(detailsQuery);
+        const found = !!post === true;
+        if (!found) throw new Error("Post not found");
+        return PostMap.toDomain(post);
+    }
+
+    public async getNumberOfCommentsByPostId(postId: PostId | string): Promise<number> {
+        postId = postId instanceof PostId
+            ? (<PostId>postId).id.toString()
+            : postId;
+
+        const result = await this.models.sequelize.query(
+            `SELECT COUNT(*)
+             FROM comment
+             WHERE post_id = "${postId}";`
+        );
+        const count = result[0][0]['COUNT(*)'];
+        return count;
+    }
+
+    public async getNumberOfFirstLevelComments(postId: PostId | string): Promise<number> {
+        postId = postId instanceof PostId
+            ? (<PostId>postId).id.toString()
+            : postId;
+
+        const result = await this.models.sequelize.query(
+            `SELECT COUNT(*)
+             FROM comment
+             WHERE post_id = "${postId}"
+               AND parent_comment_id IS NULL;`
+        );
+        const count = result[0][0]['COUNT(*)'];
+        return count;
+    }
+
+    public async getPostDetailsBySlug(slug: string, offset?: number): Promise<PostDetails> {
+        const PostModel = this.models.Post;
+        const detailsQuery = this.createBaseDetailsQuery();
+        detailsQuery.where['slug'] = slug;
+        const post = await PostModel.findOne(detailsQuery);
+        const found = !!post === true;
+        if (!found) throw new Error("Post not found");
+        return PostDetailsMap.toDomain(post)
+    }
+
+    public async getRecentPosts(offset?: number): Promise<PostDetails[]> {
+        const PostModel = this.models.Post;
+        const detailsQuery = this.createBaseDetailsQuery();
+        detailsQuery.offset = offset ? offset : detailsQuery.offset;
+        const posts = await PostModel.findAll(detailsQuery);
+        const promises: Promise<number>[] = [];
+
+        for (const post of posts) {
+            post.total_num_comments = promises.push(this.getNumberOfFirstLevelComments(post.post_id)
+                .then(count => post.total_num_comments = count));
+        }
+        await Promise.all(promises)
+        return posts.map((p) => PostDetailsMap.toDomain(p))
+    }
+
+    public async getPopularPosts(offset?: number): Promise<PostDetails[]> {
+        const PostModel = this.models.Post;
+        const detailsQuery = this.createBaseDetailsQuery();
+        detailsQuery.offset = offset ? offset : detailsQuery.offset;
+        detailsQuery['order'] = [
+            ['points', 'DESC'],
+        ];
+
+        const posts = await PostModel.findAll(detailsQuery);
+        return posts.map((p) => PostDetailsMap.toDomain(p))
+    }
+
+    public async getPostBySlug(slug: string): Promise<Post> {
+        const PostModel = this.models.Post;
+        const detailsQuery = this.createBaseQuery();
+        detailsQuery.where['slug'] = slug;
+        const post = await PostModel.findOne(detailsQuery);
+        const found = !!post === true;
+        if (!found) throw new Error("Post not found");
+        return PostMap.toDomain(post);
+    }
+
+    public async exists(postId: PostId): Promise<boolean> {
+        const PostModel = this.models.Post;
+        const baseQuery = this.createBaseQuery();
+        baseQuery.where['post_id'] = postId.id.toString();
+        const post = await PostModel.findOne(baseQuery);
+        const found = !!post === true;
+        return found;
+    }
+
+    public delete(postId: PostId): Promise<void> {
+        const PostModel = this.models.Post;
+        return PostModel.destroy({where: {post_id: postId.id.toString()}});
+    }
+
+    private saveComments(comments: Comments) {
+        return this.commentRepo.saveBulk(comments.getItems());
+    }
+
+    private savePostVotes(postVotes: PostVotes) {
+        return this.postVotesRepo.saveBulk(postVotes);
+    }
+
+    public async save(post: Post): Promise<void> {
+        const PostModel = this.models.Post;
+        const exists = await this.exists(post.postId);
+        const isNewPost = !exists;
+        const rawSequelizePost = await PostMap.toPersistence(post);
+
+        if (isNewPost) {
+
+            try {
+                await PostModel.create(rawSequelizePost);
+                await this.saveComments(post.comments);
+                await this.savePostVotes(post.getVotes());
+
+            } catch (err) {
+                await this.delete(post.postId);
+                throw new Error(err.toString())
+            }
+
+        } else {
+            // Save non-aggregate tables before saving the aggregate
+            // so that any domain events on the aggregate get dispatched
+            await this.saveComments(post.comments);
+            await this.savePostVotes(post.getVotes());
+
+            await PostModel.update(rawSequelizePost, {
+                // To make sure your hooks always run, make sure to include this in
+                // the query
+                individualHooks: true,
+                hooks: true,
+                where: {post_id: post.postId.id.toString()}
+            });
+        }
+    }
 }
